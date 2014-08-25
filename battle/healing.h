e75240d581336c932189d34cfbe74461d3a3a00b
Enrique Bonilla <enriqueBonilla93@outlook.com>
2014-08-25 00:00:34 -0500

    Deleted files

--- battle/healing.h
+++
@@ -1,9 +0,0 @@
-#ifndef HEALING_H
-#define HEALING_H
-#include "item.h"
-//Healing items are health pots and mana pots
-class healing: public item{
-	public:
-		healing(std::string n, std::string d): item(n,d){}
-};
-#endif
